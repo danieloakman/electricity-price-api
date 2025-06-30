a593a5914e4f6e92c4af8a501d7b494aaf0bc319
Daniel (Oakman) Brown <42539848+danieloakman@users.noreply.github.com>
2025-06-30 01:38:50 +0000

    Add basic python nix shell

---
+++ shell.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+(pkgs.mkShell {
+  name = "CyberCX-Task-Shell";
+  packages = with pkgs; [
+    python312
+    (python312.withPackages (ps: with ps; [
+      pip
+      virtualenv
+      venvShellHook
+    ]))
+    stdenv.cc.cc
+    glibc
+    glib.dev
+    zlib
+    cmake
+  ];
+  buildInputs = with pkgs; [
+    pkg-config
+  ];
+  shellHook = ''
+    VENV=".venv"
+    if [ ! -d "$VENV" ]; then
+      python -m venv $VENV
+    fi
+    source ./$VENV/bin/activate
+    export SHELL=${pkgs.zsh}/bin/zsh
+    exec $SHELL
+  '';
+})
